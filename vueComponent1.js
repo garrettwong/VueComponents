b76412f2e53f544055805e486ea2decbb5c829a2
garrettwong <gwong005@gmail.com>
2017-09-11 19:56:24 -0700

    update to use public .json file

--- vueComponent1.js
+++ vueComponent1.js
@@ -5,11 +5,36 @@ Vue.component('todo-item', {
 
 var app7 = new Vue({
   el: '#app-7',
+  // props: ['groceryList']
   data: {
     groceryList: [
       { id: 0, text: 'Vegetables' },
       { id: 1, text: 'Cheese' },
       { id: 2, text: 'Whatever else humans are supposed to eat' }
     ]
+  },
+  mounted: function () {
+    var self = this;
+
+    let url = 'https://data.cityofnewyork.us/api/views/kku6-nxdu/rows.json?accessType=DOWNLOAD';
+    
+    $.ajax({
+      url: url, //'http://gworldapi.azurewebsites.net/api/Words/Random?count=30',
+      method: 'GET',
+      success: function (data) {
+        console.log(data);
+        let dArr = data.meta.view.columns;
+        console.log(dArr);
+        self.groceryList = dArr.map((d, idx) => {
+          return {
+            id: d.id,
+            text: d.name
+          }
+        });
+      },
+      error: function (error) {
+        console.log(JSON.stringify(error));
+      }
+    });
   }
 });
